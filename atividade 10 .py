0a8637a0a9e42b1deb604bdcfdf3f380c576403c
scandiana <166755578+scandiana@users.noreply.github.com>
2024-04-29 08:59:10 -0300

    atividade 10

---
+++ atividade 10 .py
@@ -0,0 +1,12 @@
+numero = int(input("digite um numero: "))
+
+if numero > 0 and numero % 2 == 0:
+    print("o numero é positivo e par.")
+elif numero > 0 and numero % 3 == 0:
+    print("o numero é positivo e múltiplo de três.")
+elif numero < 0 and numero % 2 != 0:
+    print("o numero é negativo e ímpar.")
+elif numero == 0:
+    print("o número é zero.")
+else:
+    print(" nao é nenhum deles")
